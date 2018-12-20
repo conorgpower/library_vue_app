455ed68770531e6e0b63422a0c1681d3cf0b6795
Conor <conorgpower@gmail.com>
2018-12-20 15:01:49 +0000

    List, increase stock, delete, update author

---
+++ src/components/Edit.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app1" class="hero">
+    <h3 class="vue-title"><i class="fa fa-money" style="padding: 3px"></i>{{messagetitle}}</h3>
+    <div class="container mt-3 mt-sm-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <template v-if="childDataLoaded">
+            <book-form :book="book" bookBtnTitle="Update Book"
+                           @book-is-created-updated="updateBook"></book-form>
+          </template>
+        </div><!-- /col -->
+      </div><!-- /row -->
+    </div><!-- /container -->
+  </div>
+</template>
+
+<script>
+import bookServices from '@/services/bookServices'
+import BookForm from '@/components/BookForm'
+
+export default {
+  data () {
+    return {
+      book: {},
+      childDataLoaded: false,
+      temp: {},
+      messagetitle: ' Update Donation '
+    }
+  },
+  components: {
+    'book-form': BookForm
+  },
+  created () {
+    this.getBook()
+  },
+  methods: {
+    getBook: function () {
+      bookServices.fetchBooks(this.$router.params)
+        .then(response => {
+          this.temp = response.data
+          this.book = this.temp[0]
+          this.childDataLoaded = true
+        })
+        .catch(error => {
+          this.errors.push(error)
+          console.log(error)
+        })
+    },
+    updateBook: function (book) {
+      bookServices.putBookAuthor(this.$router.params, book)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          this.errors.push(error)
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #app1 {
+    width: 95%;
+    margin: 0 auto;
+  }
+  .vue-title {
+    margin-top: 30px;
+    text-align: center;
+    font-size: 45pt;
+    margin-bottom: 10px;
+  }
+</style>
